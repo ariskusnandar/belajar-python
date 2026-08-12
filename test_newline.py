d38b7e7f884fcb0355824843ab0d7b321232850e
ariskusnandar <aris.kusnandar24.ak@gmail.com>
2026-08-12 18:16:17 +0700

    Hari 3 - Eksperimen newline dan tab

---
+++ test_newline.py
@@ -0,0 +1,14 @@
+# Test newline
+
+print("1. Tanpa newline: Halo Dunia")
+print("2. Dengan newline di tengah: Halo\nDunia")
+print("3. Dengan newline di awal:\nAwal baris baru")
+print("4. Dengan newline di akhir: Akhir baris baru\nSelesai")
+print("5. Dengan tab: Halo\tDunia")
+
+# (Optional) Menampilkan data dalam format tabel menggunakan \t
+print("\n")
+print("Nama\t\tUmur\t\tKota")
+print("Aris\t\t25\t\tJakarta")
+print("Budi\t\t30\t\tBandung")
+print("Citra\t\t22\t\tSurabaya")
